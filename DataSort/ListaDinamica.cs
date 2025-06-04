4bebb76e44c7ed5c4b179ad8549a4300bac2aa2c
Mari <maybrntt@icloud.com>
2025-06-03 21:57:26 -0300

    Algoritmo de ordenação (código incompleto)

---
+++ DataSort/ListaDinamica.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSort
+{
+    public class ListaDinamica<T> where T : IComparable<T>
+    {
+        private List<T> elementos; // elementos da lista
+        public int Comparacoes { get; private set; } // comparar valores
+        public int Trocas { get; private set; } // faz trocas de valores na lista
+
+        public ListaDinamica()
+        {
+            elementos = new List<T>();
+        }
+
+        public void Inserir (T valor)
+        {
+            elementos.Add(valor);
+        }
+
+        public void Exibir()
+        {
+            if (elementos.Count ==0)
+            {
+                Console.WriteLine("Lista vazia.");
+                return;
+            }
+            Console.Write("Lista: ");
+            foreach (var item in elementos)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public bool Vazia()
+        {
+            return elementos.Count == 0;
+        }
+
+        public void GerarAleatorio(int quantidade, int min, int max)
+        {
+            Random rand = new Random(); // gerar valor aleat
+            elementos.Clear(); // limpar o ultimo elemento gerado
+            for (int i = 0; i < quantidade; i++) 
+            {
+                int valor = rand.Next(min, max + 1);
+                elementos.Add((T)Convert.ChangeType(valor, typeof(T))); // "convert" é porque a lista não ta especificada para algum tipo de valor, então vai converter para "int"
+            }
+        }
+
+        public List<T> ObterTodos()
+        {
+            return new List<T>(elementos);
+        }
+
+        public ListaDinamica<T> Clonar()
+        {
+            var copia = new ListaDinamica<T>(); // clonar lista
+            foreach (var item in elementos)
+            {
+                copia.Inserir(item);
+            }
+            return copia;
+        }
+
+        private void ResetarContadores() // reseta tudo
+        {
+           Comparacoes = 0;
+           Trocas = 0;
+        }
+
+        // bubble sort 
+        public void OrdenarBubble()
+        {
+            ResetarContadores();
+            for (int i = 0; i < elementos.Count - 1;i++)
+            {
+                for (int j = 0; j < elementos.Count -1; j++) // p gerar esse for: selecionar "for loop for"
+                {
+                    Comparacoes++;
+                    if (elementos[j].CompareTo(elementos[j+1]) > 0)
+                    {
+                        Trocar(j, j + 1); // exemplo: j = 1; se 1 for maior, ele vai trocar pra 2 e vice versa
+                    }
+                }
+            }
+        }
+        // trocar
+        void Trocar(int a, int b)
+        {
+            T temp = elementos[a];
+            elementos[a] = elementos[b];
+            elementos[b] = temp;
+            Trocas++;
+        }
+    }
+}
---
+++ DataSort/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDinamica<int> lista = new ListaDinamica<int>();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Ordenar listas! <3");
+                Console.WriteLine("1 - Inserir nova lista");
+                Console.WriteLine("2 - Exibir lista atual");
+                Console.WriteLine("3 - Ordenar lista");
+                Console.WriteLine("4 - Gerar lista aleatória");
+                Console.WriteLine("0 - Sair");
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                        lista = new ListaDinamica<int>();
+                        Console.WriteLine("\n Quantos elementos deseja inserir?");
+                        if (int.TryParse(Console.ReadLine(), out int n) && n > 0) // && diz que o número inserido precisa ser maior que 0
+                        {
+                            for (int i = 0; i < n; i++)
+                            {
+                                Console.Write($"Digite o {i + 1}° valor: ");
+                                if (int.TryParse(Console.ReadLine(), out int valor))
+                                    lista.Inserir(valor);
+                                else
+                                {
+                                    Console.WriteLine("Opção inválida. D: Pressione ENTER para continuar.");
+                                    Console.ReadLine();
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Número inválido. D: Pressione ENTER para continuar.");
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("\nLista atual: ");
+                        lista.Exibir();
+                        Console.WriteLine("Pressione ENTER para continuar");
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        if (lista.Vazia())
+                        {
+                            Console.WriteLine("Lista está vazia. Insira elementos primeiro.");
+                            Console.ReadLine();
+                        }
+                        Console.WriteLine("\nEscolha um algoritmo de orientação: ");
+                        Console.WriteLine("1- Bubble Sort");
+                        Console.WriteLine("2- Selection Sort");
+                        Console.WriteLine("3- Insertion Sort");
+                        Console.WriteLine("4- Merge Sort");
+                        Console.WriteLine("5- Quick Sort");
+                        Console.WriteLine("");
+                        string escolha = Console.ReadLine();
+
+                        Console.WriteLine("\nLista antes da organização:");
+                        Stopwatch sw = Stopwatch.StartNew(); 
+
+                        var copiaOrdenavel = lista.Clonar();
+                        switch (escolha)
+                        {
+                            case "1": copiaOrdenavel.OrdenarBubble(); break;
+                            // implementar os demais (vamos fazer nas proximas aulas)
+                            case "2": copiaOrdenavel.OrdenarBubble(); break;
+                            case "3": copiaOrdenavel.OrdenarBubble(); break;
+                            case "4": copiaOrdenavel.OrdenarBubble(); break;
+                            case "5": copiaOrdenavel.OrdenarBubble(); break;
+                            default:
+                                Console.WriteLine("Opção inválida. D:");
+                                Console.ReadLine();
+                                continue;
+                        }
+
+                        sw.Stop();
+
+                        Console.WriteLine("\nLista após ordenação: ");
+                        copiaOrdenavel.Exibir();
+                        Console.WriteLine($"\nTempo de execução: {sw.ElapsedMilliseconds} segundos.");
+                        Console.WriteLine($"\nComparações: {copiaOrdenavel.Comparacoes}");
+                        Console.WriteLine($"\nTrocas: {copiaOrdenavel.Trocas}");
+                        Console.WriteLine($"\nPressione ENTER para continuar.");
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        break;
+                    case "0":
+                        Console.WriteLine("Saindo . . .");
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida! D: Pressione ENTER para continuar.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
