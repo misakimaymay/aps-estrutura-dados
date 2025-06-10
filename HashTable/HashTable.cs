f5ea35cc20f1e84585a6d4fa9e449ce30a2c67d5
Mari <maybrntt@icloud.com>
2025-06-10 20:33:16 -0300

    Tabela Hash

---
+++ HashTable/HashTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class HashTable<T>
+    {
+        private List<LinkedList<T>> tabela;
+        private int capacidade;
+
+        public HashTable(int capacidade = 10)
+        {
+            this.capacidade = capacidade;
+            tabela = new List<LinkedList<T>>(capacidade);
+
+            for (int i = 0; i < capacidade; i++)
+                tabela.Add(new LinkedList<T>());
+        }
+        
+        private int Hash(T valor)
+        {
+            return Math.Abs(valor.GetHashCode()) % capacidade; // geralmente chamada de índice
+        }
+
+        public void Inserir(T valor)
+        {
+            int indice = Hash(valor);
+            if (!tabela[indice].Contains(valor)) // o código indica que o valor existe, mas o "!" indica que o resultado é diferente (logo o código indica se o valor não existir, vai criar um valor na posição indicada)
+            {
+                tabela[indice].AddLast(valor);
+                Console.WriteLine($"{valor} inserido na posição {indice}");
+            }
+            else
+            {
+                Console.WriteLine($"{valor} já existe na posição {indice}.");
+            }
+        }
+
+        public bool Buscar(T valor)
+        {
+            int indice = Hash(valor);
+            return tabela[indice].Contains(valor);
+        }
+
+        public bool Remover(T valor)
+        {
+            int indice = Hash(valor);
+            return tabela[indice].Remove(valor);
+        }
+
+        public void Exibir()
+        {
+            for (int i = 0; i < capacidade; i++)
+            {
+                Console.Write($"[{i}]: ");
+                foreach (var item in tabela[i])
+                    Console.Write(item + " -> ");
+                Console.Write("null");
+            }
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> tabela = new HashTable<int>();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Tabela Hash! ( ˶ˆᗜˆ˵ )");
+                Console.WriteLine("1- Inserir valor: ");
+                Console.WriteLine("2- Buscar valor: ");
+                Console.WriteLine("3- Remover valor: ");
+                Console.WriteLine("4- Exibir tabela: ");
+                Console.WriteLine("0- Sair. ");
+                Console.Write("Escolha uma opção: ");
+
+                string opcao = Console.ReadLine();
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case "1":
+                        Console.Write("Valor a inserir: ");
+                        if (int.TryParse(Console.ReadLine(), out int valorInserir))
+                            tabela.Inserir(valorInserir);
+                        else
+                            Console.WriteLine("Valor inválido.");
+                        break;
+                    case "2":
+                        Console.Write("Valor a buscar: ");
+                        if (int.TryParse(Console.ReadLine(), out int valorBuscar))
+                        {
+                            if (tabela.Buscar(valorBuscar))
+                            {
+                                Console.WriteLine($"{valorBuscar} encontrado! :D");
+                            }
+                            else
+                                Console.WriteLine($"{valorBuscar} não encontrado na tabela.");
+                        }
+                        else
+                            Console.WriteLine("Valor inválido.");
+                        break;
+                    case "3":
+                        Console.Write("Valor a remover: ");
+                        if (int.TryParse(Console.ReadLine(), out int valorRemover))
+                        {
+                            if (tabela.Remover(valorRemover))
+                                Console.WriteLine($"{valorRemover} removido!");
+                            else
+                                Console.WriteLine($"{valorRemover} não encontrado. ");
+                        }
+                        else
+                            Console.WriteLine("Valor inválido.");
+                        break;
+                    case "4":
+                        Console.Write("Tabela Hash:");
+                        tabela.Exibir();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Opção inválida. ");
+                        break;
+                }
+
+                Console.WriteLine("\nPressione ENTER para continuar!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
